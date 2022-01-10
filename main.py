eac85e50ce245e9f21adfbd7b2c1b2088af41291
ofek yariv <Ofekyariv28@gmail.com>
2022-01-10 15:21:06 +0200

    open server

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def read_port():
+    with open('myoirt,info', 'r') as my_port:
+        return my_port.read()
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    port = read_port()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
